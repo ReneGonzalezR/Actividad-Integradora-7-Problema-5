8277334ca3ab49e204d141fc1fdaefd7d7982a68
Rene Gonzalez <rene_glez1992@hotmail.com>
2021-04-04 13:32:11 -0600

    Agregar archivos de proyecto.

---
+++ ReservacionService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="ReservacionService.asmx.cs" Class="Actividad_Integradora_7_Problema_5.WebService1" %>
---
+++ ReservacionService.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data.SqlClient
+
+namespace Actividad_Integradora_7_Problema_5
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+        SqlCommand conexion = new SqlCommand();
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+    }
+}
